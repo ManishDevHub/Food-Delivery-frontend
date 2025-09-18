e75f43ba46839c904427ce89600660cbe5b327f1
YourGitHubUsername <ramuk13476@gmail.com>
2025-09-18 16:38:02 +0530

    food-deli-frontend

--- fontend/src/pages/Cart/Cart.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import '../Cart/Cart.css'
-export default function Cart() {
-  return (
-    <div>Cart</div>
-  )
-}
--- fontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import '../placeOrder/placeOrder.css'
-export default function placeOrder() {
-  return (
-    <div>placeOrder</div>
-  )
-}
